40173507881fd0b958bbbda2741879c2558eca2f
hiio42O <hongsamhc2@gmail.com>
2023-01-06 18:25:45 +0900

    [TEST] Whether API

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,6 +4,7 @@
 import React from "react";
 import styled from "styled-components";
 import Nav from "@components/nav";
+import axios from "axios";
 // SVG
 
 import { FlexCenterLayout, GridColumnLayout } from "@resources/globalStyle";
@@ -23,6 +24,25 @@ const MainWrapper = styled(FlexCenterLayout)`
 `;
 
 const Main = () => {
+  axios
+    .get(
+      "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst",
+      {
+        params: {
+          serviceKey:
+            "85SVNYrwH8xXJbjJgkYoSQsBQqzKtn7WO1JdYfXeeMT37b9Br6ClP7F4gnruv/N4aY6wdkDHWNvzieim1yz98A==",
+          numOfRows: 14 * 24 * 7,
+          pageNo: 1,
+          dataType: "JSON",
+          base_date: "20230106",
+          base_time: "1700",
+          nx: "55",
+          ny: "127",
+        },
+      }
+    )
+    .then((r) => console.log(r));
+
   return (
     <MainWrapper>
       <div>
